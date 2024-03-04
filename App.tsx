9c736eec87170d0759c7a0654faa60dd7e801c58
Andrei <andrei.cherciu24@gmail.com>
2024-03-04 20:43:24 +0000

    initial commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { Text, SafeAreaView, Button } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView className="flex-1 justify-center items-center bg-blue-400 text-white">
+      <Text className="text-5xl text-white font-black ">Hello Angelo</Text>
+    </SafeAreaView>
+  );
+}
