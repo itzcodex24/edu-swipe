95d080aa40cc5c5866d64f44fc9dc8d46524deff
Andrei <andrei.cherciu24@gmail.com>
2024-03-05 18:17:27 +0000

    fix: version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        monument: ["MonumentExtended-Ultrabold", "sans-serif"],
+      },
+      dropShadow: {
+        mainbutton: "0 35px 35px rgba(200, 20, 20, 0.25)",
+      },
+    },
   },
   plugins: [],
 };
